78d3af73465b8478d73135443c6e69da5887540c
TheComputerCat <jusanchez@unal.edu.co>
2023-04-04 18:57:03 -0500

    Refactor

--- kata2/code.js
+++ kata2/code.js
@@ -1,69 +1,50 @@
 export function createGird(height, width) {
-    const newGrid = {
+    return {
         width: width,
         value: new Array(height * width).fill(false)
     };
-    return newGrid;
 }
 export function setCellAsAlive(grid, row, column){
     let newGrid = structuredClone(grid);
     newGrid.value[newGrid.width * row + column] = true;
     return newGrid;
 }
-export function setCellAsDead(grid, row, column){
-    let newGrid = structuredClone(grid);
-    newGrid.value[newGrid.width * row + column] = false;
-    return newGrid;
-}
 export function isAlive(grid, row, column){
     return grid.value[grid.width * row + column];
 }
 export function checkUnderpopulated(grid, row, column){
-    if(isAlive(grid, row, column) && countAliveNeighbors(grid, row, column) < 2){
-        return true;
-    }
-    return false;
+    return isAlive(grid, row, column) && countAliveNeighbors(grid, row, column) < 2;
+}
+function getCoordinatesFromIndex(grid, index){
+    return [Math.floor(index/grid.width), index%grid.width];
 }
 function countAliveNeighbors(grid, row, column){
-    const getIndexFromOffset = (offset) => offset + grid.width * row + column;
-    const neighborUp = [-grid.width - 1, -grid.width, -grid.width + 1].map(getIndexFromOffset);
-    const neighborMiddel = [ -1, 1].map(getIndexFromOffset);
-    const neighborBottom = [grid.width - 1, grid.width, grid.width + 1].map(getIndexFromOffset);
-
-    const aliveNeighborUp = neighborUp.filter((cellIndex) => Math.floor(cellIndex/grid.width) === row - 1);
-    const aliveNeighborMiddel = neighborMiddel.filter((cellIndex) => Math.floor(cellIndex/grid.width) === row);
-    const aliveNeighborBottom = neighborBottom.filter((cellIndex) => Math.floor(cellIndex/grid.width) === row + 1);
-
-    const neighbors = aliveNeighborUp.concat(aliveNeighborMiddel, aliveNeighborBottom);
-
-    const aliveNeighbors = neighbors.filter((neighbor) => isAlive(grid, Math.floor(neighbor/grid.width), neighbor%grid.width));
+    return getRowAliveNeighbours(grid, row, column,[-grid.width - 1, -grid.width, -grid.width + 1], row - 1)
+        + getRowAliveNeighbours(grid, row, column,[ -1, 1], row) +
+        getRowAliveNeighbours(grid, row, column,[grid.width - 1, grid.width, grid.width + 1], row + 1);
+}
+function getRowAliveNeighbours(grid, row, column, offsetsArray, expectedOffsetsRow){
+    const neighbors = offsetsArray.map((offset) => offset + grid.width * row + column);
+    const correctNeighbours = neighbors.filter((cellIndex) => Math.floor(cellIndex/grid.width) === expectedOffsetsRow);
+    const aliveNeighbors = correctNeighbours.filter((cellIndex) => isAlive(grid, ...getCoordinatesFromIndex(grid, cellIndex)))
     return aliveNeighbors.length;
 }
 export function checkOvercrowding(grid, row, column){
-    if(isAlive(grid, row, column) && countAliveNeighbors(grid, row, column) > 3){
-        return true;
-    }
-    return false;
+    return isAlive(grid, row, column) && countAliveNeighbors(grid, row, column) > 3;
 }
 export function checkLivesNextGeneration(grid, row, column){
     const aliveNeighbors = countAliveNeighbors(grid, row, column);
-    if(isAlive(grid, row, column) && (aliveNeighbors == 2 || aliveNeighbors == 3)){
-        return true;
-    }
-    return false;
+    return isAlive(grid, row, column) && (aliveNeighbors === 2 || aliveNeighbors === 3);
 }
 export function checkDeadCellLives(grid, row, column){
-    if(!isAlive(grid, row, column) && countAliveNeighbors(grid, row, column) === 3){
-        return true;
-    }
-    return false;
+    return !isAlive(grid, row, column) && countAliveNeighbors(grid, row, column) === 3;
 }
 export function calculateNextGeneration(grid){
     const width = grid.width;
     const height = grid.value.length/grid.width;
     let newGrid = createGird(height, width);
     grid.value.forEach((cell, index) => {
-        newGrid = selectCase(grid, Math.floor(index/width), index%width, newGrid);
+        newGrid = selectCase(grid, ...getCoordinatesFromIndex(grid, index), newGrid);
     });
     return newGrid;
     
